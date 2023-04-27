d621c1c055c7daf34a945545d56c2980836808a2
Antonio Brown <Brownantonio001@gmail.com>
2023-04-27 12:45:50 -0500

    Generated Sql files

---
+++ dbgen/admin.sql
@@ -0,0 +1,5 @@
+INSERT INTO Students VALUES(620145633, 'Antonio', ' Brown');
+INSERT INTO Students VALUES(620150054, 'Kyle ', 'Campbell');
+INSERT INTO Students VALUES(620141584, 'Nathaniel ', 'Dakin');
+INSERT INTO Students VALUES(620146841, 'Amelia ', 'Pellington');
+INSERT INTO Students VALUES(620107689, 'Shanique ', 'Dillon');
--- dbgen/master.py
+++ dbgen/master.py
@@ -1,7 +1,7 @@
 import psycopg2 as psql
 
 conn = psql.connect(
-    database='lecturers_db',
+    database='Clonevle_db',
     user='postgres',
     password='3072001',
     host='localhost',
@@ -17,13 +17,33 @@ with open('Lecturers.csv', 'r') as csv_file, open('lecturers.sql', 'w') as sql_f
           values = line.strip().split(",")
           insert_query = "INSERT INTO Lecturers VALUES({}, '{}', '{}', '{}', '{}');\n".format(values[0], values[1], values[2], values[3], values[4])
           sql_file.write(insert_query)
+print("A lecturer.sql database SQL file was created with data")
 
-print("A lecturer database SQL file was created with data")
+with open('Students.csv', 'r') as csv_file, open('students.sql', 'w') as sql_file:
+    for line in csv_file.readlines()[1:]:
+          values = line.strip().split(",")
+          insert_query = "INSERT INTO Students VALUES({}, '{}', '{}', '{}', '{}');\n".format(values[0], values[1], values[2], values[3], values[4])
+          sql_file.write(insert_query)
+print("A student.sql database SQL file was created with data")
+
+with open('Courses.csv', 'r') as csv_file, open('courses.sql', 'w') as sql_file:
+    for line in csv_file.readlines()[1:]:
+        values = line.strip().split(",")
+        insert_query = "INSERT INTO Course VALUES({}, '{}', '{}', '{}');\n".format(values[0], values[1], values[2], values[3])
+        sql_file.write(insert_query)
+    print("A course.sql database SQL file was created with data")
+
+with open('Admin.csv', 'r') as csv_file, open('admin.sql', 'w') as sql_file:
+    for line in csv_file.readlines()[1:]:
+        values = line.strip().split(",")
+        insert_query = "INSERT INTO Students VALUES({}, '{}', '{}');\n".format(values[0], values[1], values[2])
+        sql_file.write(insert_query)
+    print("An admin.sql database SQL file was created with data")
 
 
 
 # Close the database connection
 conn.close()
 cur.close()
-
+#cursor.close()
 
